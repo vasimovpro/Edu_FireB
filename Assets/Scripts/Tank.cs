b948294ccde8d3ddd646144c64e07a21031d2580
Denis Vasimov <dvasimov@gmail.com>
2023-05-05 21:42:11 +0500

    Added tank recoil

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Tank : MonoBehaviour
 {
     [SerializeField] private Transform _shootPoint;
     [SerializeField] private Bullet _bullet;
     [SerializeField] private float _delayBetweenShoots;
+    [FormerlySerializedAs("_recoil")] [SerializeField] private float _recoilDistance;
 
     private float _timeAfterShoot;
 
@@ -20,6 +23,9 @@ public class Tank : MonoBehaviour
             if (_timeAfterShoot > _delayBetweenShoots)
             {
                 Shoot();
+                transform.DOMoveZ(transform.position.z - _recoilDistance,
+                        _delayBetweenShoots / 2)
+                    .SetLoops(2, loopType: LoopType.Yoyo);
                 _timeAfterShoot = 0;
             }
         }
